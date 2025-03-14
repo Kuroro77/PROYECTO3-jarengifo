4b3fff675385187fccc2e80f4369f62427327d00
Jhon Rengifo <jrengifo@famisanar.com.co>
2025-03-13 20:53:33 -0500

    Configuracion ruta inicial de login metodo de autenticacion y cierre de sesion, con mensaje de bienvenida o error en caso de no existir usuario en bd

---
+++ controllers/access_controller.py
@@ -0,0 +1,29 @@
+from config.auth import login_manager
+from flask import Blueprint, render_template, request
+from models.usuarios import Usuarios
+from flask_login import current_user, logout_user
+
+home_blueprint = Blueprint("home", __name__)
+@login_manager.user_loader
+def load_user(user_id:int):
+    return Usuarios.query.get(int(user_id))
+
+@home_blueprint.route('/')
+def login():
+    return render_template('access/login.html')
+@home_blueprint.route('/auth', methods=['POST'])
+def auth():
+    username = request.form.get("username")
+    password = request.form.get("password")
+
+    user = Usuarios.auth(username, password)
+
+    if user:
+        return render_template('access/bienvenida.html', current_user=current_user)
+    else:
+        return render_template('access/404.html')
+
+@home_blueprint.route('/logout')
+def logout():
+    logout_user()
+    return render_template('access/login.html')
--- models/usuarios.py
+++ models/usuarios.py
@@ -5,3 +5,7 @@ class Usuarios(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(16), unique=True, nullable=False)
     password = db.Column(db.String(16), nullable=False)
+
+    def auth(username, password):
+        user = Usuarios.query.filter_by(username=username, password=password).first()
+        return user
